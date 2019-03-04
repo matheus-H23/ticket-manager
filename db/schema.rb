8d44459607406db8745a89c8efe42678600b7bc4
Matheus Henrique Silva <matheushmms@hotmail.com>
2019-03-04 09:52:47 -0300

    Adding Statuses to Tickets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_03_142629) do
+ActiveRecord::Schema.define(version: 2019_03_04_124339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,12 +21,20 @@ ActiveRecord::Schema.define(version: 2019_03_03_142629) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "statuses", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tickets", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "kind_id"
+    t.bigint "status_id"
     t.index ["kind_id"], name: "index_tickets_on_kind_id"
+    t.index ["status_id"], name: "index_tickets_on_status_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +56,5 @@ ActiveRecord::Schema.define(version: 2019_03_03_142629) do
   end
 
   add_foreign_key "tickets", "kinds"
+  add_foreign_key "tickets", "statuses"
 end
