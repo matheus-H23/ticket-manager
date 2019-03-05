3fceb341d43880e2ed9910276a95f41ec9b4faa0
Matheus Henrique Silva <matheushmms@hotmail.com>
2019-03-05 12:45:02 -0300

    Config routes Manager and account.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   get 'home/index'
   devise_for :users, path: 'auth'
   resources :tickets
   resources :kinds
   resources :statuses
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :manager do
+    resources :tickets
+  end
+
+  namespace :account do
+    resources :tickets
+  end
 
   root "home#index"
 
