aa2edd98627c103dc359ec7a4721724bdc5b0385
Matheus Henrique Silva <matheushmms@hotmail.com>
2019-03-07 21:58:22 -0300

    Add fields Assignee and Reporter to ticket.

--- db/migrate/20190304130326_add_fields_to_ticket.rb
+++ db/migrate/20190304130326_add_fields_to_ticket.rb
@@ -1,6 +1,6 @@
 class AddFieldsToTicket < ActiveRecord::Migration[5.2]
   def change
-    add_column :tickets, :assignee, :integer
-    add_column :tickets, :reporter, :integer
+    add_column :tickets, :assignee_id, :integer
+    add_column :tickets, :reporter_id, :integer
   end
 end
